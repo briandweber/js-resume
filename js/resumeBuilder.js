0e3eaa05a422bfa2802350ebffc56899cbeb9e08
Brian Weber <briandweber@gmail.com>
2017-08-10 00:53:46 -0700

    added loops for appending objects to index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -22,7 +22,7 @@ var bio = {
   "display": function(){}
 };
 
-var formattedPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
 $("#header").prepend(formattedPic);
 
 var education = {
@@ -34,7 +34,6 @@ var education = {
       "majors": ["Economics"],
       "dates": "June/2011 - May/2013",
       "url": "https://www.unl.edu",
-      "online": "https://www.unl.edu"
     }
   ],
   "onlineCourses": [
@@ -45,8 +44,22 @@ var education = {
       "url": "https://www.udacity.com"
     }
   ],
-  "display": function(){}
+  "display": function(){
+    for (item in education.schools){
+      $("#education").append(HTMLschoolStart);
+      var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[item].name);
+      var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[item].location);
+      $(".education-entry:last").append(formattedSchoolName);
+      $(".education-entry:last").append(formattedSchoolLocation);
+      var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[item].dates);
+      $(".education-entry:last").append(formattedSchoolDates);
+      var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[item].majors);
+      $(".education-entry:last").append(formattedSchoolMajor);
+      var formattedSchool
+    }
+  }
 }
+education.display();
 
 var work = {
   "jobs": [
@@ -58,8 +71,21 @@ var work = {
         "description": "Responsibilities include working closely with physicians and office/business manager for overall management of clinic’s patient record keeping system in order to facilitate the efficient and thorough medical treatment of patients."
       }
   ],
-  "display": function(){}
+  "display": function(){
+    for (job in work.jobs){
+      $("#workExperience").append(HTMLworkStart);
+      var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].  employer);
+      var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+      var formattedEmployerTitle = formattedEmployer + formattedTitle;
+      $(".work-entry:last").append(formattedEmployerTitle);
+      var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+      $(".work-entry:last").append(formattedDates);
+      var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[ job].description);
+      $(".work-entry:last").append(formattedDescription);
+    }
+  }
 };
+work.display();
 
 var projects = {
   "projects": [
@@ -83,24 +109,9 @@ if (bio.skills.length > 0){
   $("#skills").append(formattedSkill);
 }
 
-function displayWork(){
-    for (job in work.jobs){
-    $("#workExperience").append(HTMLworkStart);
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedEmployerTitle = formattedEmployer + formattedTitle;
-    $(".work-entry:last").append(formattedEmployerTitle);
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    $(".work-entry:last").append(formattedDates);
-    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    $(".work-entry:last").append(formattedDescription);
-  }
-}
-displayWork();
-
-$("#main").append(internationalizeButton);
-function inName(name){
-  var arr = name.split(" ");
-  var lastName = arr[1].toUpperCase();
-  return arr[0] + " " + lastName;
-}
+// $("#main").append(internationalizeButton);
+// function inName(name){
+//   var arr = name.split(" ");
+//   var lastName = arr[1].toUpperCase();
+//   return arr[0] + " " + lastName;
+// }
